60635f4f1c73d5495e6648616bc4a78c4dc1a66f
pinkp00 <pinkp3786@gmail.com>
2026-07-06 12:42:05 +0000

    ....

---
+++ azhly_mobile/android/app/src/main/kotlin/com/example/azhly_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.azhly_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- azhly_mobile/lib/views/auth/splash_screen.dart
+++ azhly_mobile/lib/views/auth/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
@@ -10,15 +11,30 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
+    // 3 seconds ke baad login page par move ho jao
+    Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacementNamed(context, '/login');
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF0B1B3D),
-      body: Center(child: Text("AZHly Logo", style: TextStyle(color: Colors.white, fontSize: 30))),
+    return Scaffold(
+      backgroundColor: const Color(0xFF0B1B3D), // Dark Blue Theme
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Yahan apna logo lagao
+            const Icon(Icons.school, size: 100, color: Colors.purple),
+            const SizedBox(height: 20),
+            const Text(
+              "AZHly System",
+              style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
